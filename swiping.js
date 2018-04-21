1d547b6c019c807a882ce9474313ddf6bac54438
TSing19 <tomer.singal1275@gmail.com>
2018-04-21 16:52:01 -0400

    fixed another merge issue

--- swiping.js
+++ swiping.js
@@ -1,35 +1,35 @@
-var $card, $resume, $bio, $info;
-
-
+let $card, $resume, $bio;
 let resumeOpacity = 1;
 let opacityUpdate = 0.1;
 let disappearPoint = 400;
 let scrolled = 0;
 
 function scroll() {
+  console.log("scrolled");
+  scrolled = $card[0].scrollTop;
   $resume.style.opacity = "" + ((disappearPoint - scrolled) / disappearPoint);
   $bio.style.opacity = "" + (scrolled / disappearPoint);
-  scrolled = $info.scrollTop;
 }
 
 function nope() {
-  $card.addClass('noped')
+  $card.addClass('noped');
   setTimeout(function () { generateCard('last was noped') }, 500);
 }
 
 function like() {
-  $card.addClass('liked')
+  $card.addClass('liked');
   setTimeout(function () { generateCard('last was liked') }, 500);
 }
 
 function generateCard(name) {
-  var template = Handlebars.compile(document.getElementById('card-template').innerHTML);
-  document.getElementById('card-spot').innerHTML = template({ name: name })
+  let template = Handlebars.compile(document.getElementById('card-template').innerHTML);
+  document.getElementById('card-spot').innerHTML = template({ name: name });
 
   $card = $('#card');
   $resume = $('.pdf')[0];
   $bio = $('.bio')[0];
-  $card.onscroll = scroll;
+  console.log($card);
+  $card[0].onscroll = scroll;
 }
 
-generateCard('first')
+generateCard('first');
