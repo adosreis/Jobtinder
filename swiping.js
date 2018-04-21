a5f099de96dc14847931616bcdf4a5a17f7c5236
Matthew Ramina <mattramina@gmail.com>
2018-04-21 16:41:00 -0400

    fixed some merge issues

--- swiping.js
+++ swiping.js
@@ -1,33 +1,35 @@
-let $resume = $('.pdf')[0];
-let $bio = $('.bio')[0];
-let $info = $('.person-card')[0];
+var $card, $resume, $bio, $info;
+
+
 let resumeOpacity = 1;
 let opacityUpdate = 0.1;
 let disappearPoint = 400;
 let scrolled = 0;
 
-$info.onscroll = scroll;
 function scroll() {
   $resume.style.opacity = "" + ((disappearPoint - scrolled) / disappearPoint);
   $bio.style.opacity = "" + (scrolled / disappearPoint);
   scrolled = $info.scrollTop;
 }
 
-function nope(){
-    var $card = $('#card');
-    $card.addClass('noped')
-    setTimeout(function(){generateCard('last was noped')}, 500);
+function nope() {
+  $card.addClass('noped')
+  setTimeout(function () { generateCard('last was noped') }, 500);
 }
 
-function like(){
-    var $card = $('#card');
-    $card.addClass('liked')
-    setTimeout(function(){generateCard('last was liked')}, 500);
+function like() {
+  $card.addClass('liked')
+  setTimeout(function () { generateCard('last was liked') }, 500);
 }
 
-function generateCard(name){
-    var template = Handlebars.compile(document.getElementById('card-template').innerHTML);
-    document.getElementById('card-spot').innerHTML = template({name: name})
+function generateCard(name) {
+  var template = Handlebars.compile(document.getElementById('card-template').innerHTML);
+  document.getElementById('card-spot').innerHTML = template({ name: name })
+
+  $card = $('#card');
+  $resume = $('.pdf')[0];
+  $bio = $('.bio')[0];
+  $card.onscroll = scroll;
 }
 
 generateCard('first')
