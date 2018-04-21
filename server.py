c63db6b243fa55aa583495871d0c4fc9a695e0dc
Andrew Dos Reis <andrew.dosreis@rutgers.edu>
2018-04-21 23:25:11 +0000

    util with insert for applicants done

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import *
 from flask_mysqldb import MySQL
 import pandas as pd
 import random
+import util
 #import lol_classifier
 
 
@@ -9,20 +10,20 @@ app = Flask(__name__)
 mysql = MySQL(app)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_DB'] = 'schema'
+app.config['MYSQL_DB'] = 'Hackru'
 app.config['MYSQL_PASSWORD'] = 'Nitro06snow'
-
 
 
 @app.route('/')
 def index():
 	return render_template('index.html')
 
-@app.route('/filter')
-def applicant_filter():
+#@app.route('/filter')
+#def applicant_filter():
 
-@app.route('/viewer'):
-def applicant_viewer
+
+#@app.route('/viewer'):
+#def applicant_viewer
 
 
 if __name__ == '__main__':
--- util.py
+++ util.py
@@ -1,12 +1,93 @@
 import requests
+import csv
+import MySQLdb
+import random
+
+
+MYSQL_HOST = 'localhost'
+MYSQL_USER = 'root'
+MYSQL_DB = 'Hackru'
+MYSQL_PASSWORD = 'Nitro06snow'
 
 ICIMS_API_KEY = '$2a$12$Kp8l3eJW3GVGaCOlBqY1j.s6O4fRU21c7P29p4KbuSRAztb9JU2Ee'
 ICIMS_HEADER = {'Authorization': 'Bearer {}'.format(ICIMS_API_KEY)}
-ICIMS_COMPANY = 2040
+ICIMS_COMPANY = 2046
 ICIMS_BASE_URL = 'https://hackicims.com/api/v1/companies/{}/'.format(ICIMS_COMPANY)
 
 def get_jobs():
-    return requests.get(ICIMS_BASE_URL + 'jobs', headers=ICIMS_HEADER).json()
+    try:
+        return requests.get(ICIMS_BASE_URL + 'jobs', headers=ICIMS_HEADER).json()
+    except HTTPError as err:
+        if err.response.status_code == 404:
+            print('api request not found error')
+        else:
+            print('unknown error occured')
+        return None
+
 
 def get_people():
-    return requests.get(ICIMS_BASE_URL + 'people', headers=ICIMS_HEADER).json()
+    try:
+        print("get_people")
+        return requests.get(ICIMS_BASE_URL + 'people', headers=ICIMS_HEADER).json()
+    except HTTPError as err:
+        if err.response.status_code == 404:
+            print('api request not found error')
+        else:
+            print('unknown error occured')
+        return None
+
+
+def applicant_upload(json):
+    print("starting upload")
+    mydb = MySQLdb.connect(host=MYSQL_HOST,
+        user=MYSQL_USER,
+        passwd=MYSQL_PASSWORD,
+        db=MYSQL_DB,use_unicode=True, charset="utf8")
+    cursor = mydb.cursor()
+    applicants = []
+    for applicant in json:
+        print(applicant['id'])
+        try:
+            if not applicant['id'] in applicant:
+                applicants.append(applicant['id'])
+                address = applicant['address']
+                address_country = address['country']
+                address_state = address['state']
+                address_city = address['city']
+                sqlApp = "INSERT INTO applicants (id, email, firstName, lastName, address_country, address_state, address_city) VALUES (%s,%s,%s,%s,%s,%s,%s)"
+
+                valuesApp = (applicant['id'],applicant['email'],applicant['firstName'],applicant['lastName'],address_country, address_state, address_city)
+                #print(valuesApp)
+                cursor.execute(sqlApp, valuesApp)
+                for edu in applicant['education']:
+                    school = edu['school']
+                    degree = edu['degree']
+                    major = edu['major']
+                    gradDate = edu['graduationDate']
+                    gpa = round(random.uniform(2.5,4.0), 2)
+                    sqlEdu = "INSERT INTO applicantEdu (id, school, degree, major, gradDate, GPA) VALUES (%s,%s,%s,%s,%s,%s)"
+                    valuesEdu = (applicant['id'],school, degree, major, gradDate, gpa)
+                    #print(valuesEdu)
+                    cursor.execute(sqlEdu, valuesEdu)
+                for skill in applicant['skills']:
+                    name = skill['name']
+                    sqlSkills = "INSERT INTO applicantSkill (id, name) VALUES (%s,%s)"
+                    valuesSk = (applicant['id'],name)
+                    #print(valuesSk)
+                    cursor.execute(sqlSkills, valuesSk)
+            else:
+                continue
+        except:
+            print("stupid name")
+            continue
+
+    #close the connection to the database.
+    mydb.commit()
+    cursor.close()
+
+if __name__ == '__main__':
+    people = get_people()
+    applicant_upload(people)
+    #(id, email,firstName, lastName, address_country, address_state, address_city)
+    #(id, school, degree, major, gradDate, GPA)
+    #(id, skill)
