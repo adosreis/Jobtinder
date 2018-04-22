7f2a861a469ac9dec0cdd80f3132e2247ffc9c23
Andrew Dos Reis <andrew.dosreis@rutgers.edu>
2018-04-22 11:02:50 +0000

    swiping yaayyyyyyyy

--- static/swiping.js
+++ static/swiping.js
@@ -1,6 +1,7 @@
 let $card, $resume, $bio;
 let disappearPoint = 300;
 let scrolled = 0;
+let personId = 0;
 
 function scroll() {
   scrolled = $card[0].scrollTop;
@@ -12,17 +13,32 @@ function scroll() {
 
 function nope() {
   $card.addClass('noped');
-  setTimeout(function () { generateCard('last was noped') }, 500);
+  sendSwipe(0);
+  //setTimeout(function () { generateCard('last was noped') }, 500);
 }
 
 function like() {
   $card.addClass('liked');
-  setTimeout(function () { generateCard('last was liked') }, 500);
+  sendSwipe(1);
+  //setTimeout(function () { generateCard('last was liked') }, 500);
+}
+
+function sendSwipe(swipe){
+  $.ajax({
+    url: location.href + '/swipe',
+    data: {'status': swipe, 'applicantId': personId},
+    method: 'POST',
+    success: function(data){
+      generateCard(JSON.parse(data));
+    }
+  });
 }
 
 function generateCard(person) {
+  console.log(person)
   let template = Handlebars.compile(document.getElementById('card-template').innerHTML);
-  document.getElementById('card-spot').innerHTML = template({ name: person.firstName + ' ' + person.lastName });
+  document.getElementById('card-spot').innerHTML = template({ name: person.firstName + ' ' + person.lastName, skills: person.skills, location: person.address_city + ', ' + person.address_state + ', ' + person.address_country });
+  personId = person.id;
 
   $card = $('#card');
   $resume = $('.pdf')[0];
@@ -30,4 +46,4 @@ function generateCard(person) {
   $card[0].onscroll = scroll;
 }
 
-generateCard('first');
+//generateCard('first');
