0c64f70320777b94e167950761b4c1962da80b28
Andrew Dos Reis <andrew.dosreis@rutgers.edu>
2018-04-22 09:16:19 +0000

    light at the end of the tunnel dimming

--- server.py
+++ server.py
@@ -3,9 +3,9 @@ import pandas as pd
 import random
 import MySQLdb
 import util
-#import lol_classifier
-
 import flask_login
+import pickle
+import MySQLdb.cursors
 
 
 
@@ -14,16 +14,15 @@ MYSQL_HOST = 'localhost'
 MYSQL_USER = 'root'
 MYSQL_DB = 'Hackru'
 MYSQL_PASSWORD = 'Nitro06snow'
-# mysql = MySQL(app)
-# app.config['MYSQL_HOST'] = 'localhost'
-# app.config['MYSQL_USER'] = 'root'
-# app.config['MYSQL_DB'] = 'Hackru'
-# app.config['MYSQL_PASSWORD'] = 'Nitro06snow'
+
+jobDB = {}
+jobDB_File = 'jobDB.pickle'
 
 mydb = MySQLdb.connect(host=MYSQL_HOST,
 	user=MYSQL_USER,
 	passwd=MYSQL_PASSWORD,
-	db=MYSQL_DB,use_unicode=True, charset="utf8")
+	db=MYSQL_DB,use_unicode=True, charset="utf8",
+	cursorclass=MySQLdb.cursors.DictCursor)
 
 app = Flask(__name__)
 app.secret_key = 'ej&e2gw*^e0i_&_0^ws)yk)d$th!kz2zd&0rx=syf6s8+p-r3q'
@@ -32,6 +31,52 @@ login_manager.init_app(app)
 
 class User(flask_login.UserMixin):
 	pass
+
+def update(id):
+	major = jobDB[id][2]
+	skill = jobDB[id][3]
+	print("update {}".format(id))
+	cursor = mydb.cursor()
+	if not major and not skill:
+		sql = "select distinct * from applicants"
+		cursor.execute(sql)
+	elif major and not skill:
+		sql = "select distinct applicants.* from applicants, applicantEdu where applicant.id = applicantEdu.id and applicantEdu.major = %s"
+		values = (major,)
+		cursor.execute(sql, values)
+	elif not major and skill:
+		sql = "select distinct applicants.* from applicants, applicantSkill where applicant.id = applicantSkill.id and applicantSkill.name = %s"
+		values = (skill,)
+		cursor.execute(sql, values)
+	else:
+		sql = "select distinct applicants.* from applicants, applicantSkill, applicantEdu where applicant.id = applicantSkill.id and applicant.id = applicantEdu.id and applicantSkill.name = %s and applicantEdu.major = %s"
+		values = (skill,major)
+		cursor.execute(sql, values)
+	rv = cursor.fetchall()
+	for applicant in rv:
+		print(applicant)
+		sql = "select School, degree, major, gradDate, GPA from applicantEdu where id = %s"
+		values = (applicant['id'],)
+		cursor.execute(sql, values)
+		eduRv = cursor.fetchall()
+		applicant['education'] = []
+		for education in eduRv:
+			applicant['education'].append(education)
+		sql = "select name from applicantSkill where id = %s"
+		value = (applicant['id'],)
+		cursor.execute(sql, values)
+		skRv = cursor.fetchall()
+		applicant['skills'] = []
+		for skill in skRv:
+			applicant['skills'].append(skill)
+		applicant = json.dumps(applicant)
+		if not applicant in jobDB[id][0]:
+			jobDB[id][0].append(applicant)
+		else:
+			continue
+	with open(jobDB_File, 'wb') as f:
+		pickle.dump(jobDB, f)
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -43,12 +88,12 @@ def load_user(user_id):
 		user.id = user_id
 		return user
 	return None
-	
+
 @app.route('/login', methods=['GET', 'POST'])
 def login_page():
 	if request.method == 'GET':
 		return app.send_static_file('Login.html')
-	
+
 	sql = 'SELECT id, email, password FROM users WHERE email = %s'
 	cursor = mydb.cursor()
 	cursor.execute(sql, (request.form['email'],))
@@ -68,7 +113,7 @@ def login_page():
 def logout():
 	flask_login.logout_user()
 	return redirect('/')
-			
+
 
 @app.route('/signup', methods = ['GET', 'POST'])
 def create_user():
@@ -85,15 +130,15 @@ def create_user():
 		cursor.close()
 		if int(data['compOrApp']) is 0:
 			id = cursor.lastrowid
-			return redirect(url_for('.create_comp', compId = id))
+			return redirect("/create_job")
 		else:
-			return redirect("/create_app", code=302)
+			return redirect("/create_app")
 	else:
 		print("BAD NEWS")
 
 
-@app.route('/create_comp', methods = ['GET','POST'])
-def create_comp(compId):
+@app.route('/make_job', methods = ['GET','POST'])
+def create_job():
 	if request.method == 'GET':
 		return app.send_static_file('jobPost.html')
 	elif request.method ==  'POST':
@@ -101,11 +146,16 @@ def create_comp(compId):
 		data = request.form
 		print(data)
 		sql = "INSERT INTO jobs (companyId, companyName, title, description, email, location_country, location_state, location_city) VALUES (%s,%s,%s, %s, %s, %s, %s, %s)"
-		values = (data['companyId'], data['companyName'], data['title'], data['description'], data['email'], data['location_country'], data['location_state'], data['location_city'])
+		values = (flask_login.current_user.get_id(), data['companyName'], data['title'], data['description'], data['email'], data['location_country'], data['location_state'], data['location_city'])
+		cursor.execute(sql, values)
+		id = cursor.lastrowid
+		sql = "INSERT INTO jobFilters (jobId, major, skill) VALUES (%s,%s,%s)"
+		values = (id, data['major'], data['skill'])
 		cursor.execute(sql, values)
 		mydb.commit()
 		cursor.close()
-		id = cursor.lastrowid
+		jobDB[id] = [[], 0, data['major'], data['skill']]
+
 		return redirect(url_for('.applicant_viewer', job_id = id))
 	else:
 		print("BAD NEWS")
@@ -119,9 +169,14 @@ def index():
 #def applicant_filter():
 
 
-@app.route('/viewer/<job_id>',)
+@app.route('/viewer/<job_id>',methods = ['GET'])
 def applicant_viewer(job_id):
-		print(job_id)
+	job_id = int(job_id)
+	update(job_id)
+	cur = jobDB[job_id][0][jobDB[job_id][1]]
+	jobDB[job_id][1] += 1
+	return render_template("viewer.html")
+
 
 
 @app.route('/viewer/<job_id>/<applicant_id>', methods = ['POST'])
@@ -129,4 +184,6 @@ def applicant_viewer_swipe_confirm(job_id,applicant_id):
 		print(job_id, applicant_id)
 
 if __name__ == '__main__':
+	with open(jobDB_File, 'rb') as f:
+		jobDB = pickle.load(f)
 	app.run(debug=True, host='0.0.0.0')
