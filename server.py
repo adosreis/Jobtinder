eaad5b0dc339ef8cca243a3e7a0c554460f50158
Andrew Dos Reis <andrew.dosreis@rutgers.edu>
2018-04-22 01:22:21 +0000

    started on sign-up, focusing on companies right now

--- server.py
+++ server.py
@@ -1,30 +1,90 @@
 from flask import *
-from flask_mysqldb import MySQL
 import pandas as pd
 import random
+import MySQLdb
 import util
 #import lol_classifier
 
 
-app = Flask(__name__)
-mysql = MySQL(app)
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_DB'] = 'Hackru'
-app.config['MYSQL_PASSWORD'] = 'Nitro06snow'
 
+
+MYSQL_HOST = 'localhost'
+MYSQL_USER = 'root'
+MYSQL_DB = 'Hackru'
+MYSQL_PASSWORD = 'Nitro06snow'
+app = Flask(__name__)
+# mysql = MySQL(app)
+# app.config['MYSQL_HOST'] = 'localhost'
+# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_DB'] = 'Hackru'
+# app.config['MYSQL_PASSWORD'] = 'Nitro06snow'
+
+mydb = MySQLdb.connect(host=MYSQL_HOST,
+	user=MYSQL_USER,
+	passwd=MYSQL_PASSWORD,
+	db=MYSQL_DB,use_unicode=True, charset="utf8")
+
+@app.route('/login')
+def login_page():
+	return render_template('login.html')
+
+@app.route('/signup', methods = ['GET', 'POST'])
+def create_user():
+	cursor = mydb.cursor()
+	if request.method == 'GET':
+		return render_template('signup.html')
+	elif request.method ==  'POST':
+		data = request.form
+		print(data)
+		sql = "INSERT INTO users (email, password, compOrApp) VALUES (%s,%s,%s)"
+		values = (data['email'],data['password'],data['compOrApp'])
+		cursor.execute(sql, values)
+		if data['compOrApp'] is 0:
+			id = cursor.lastrowid
+			return redirect(url_for('.create_comp', compId = id))
+		else:
+			return redirect("/create_app", code=302)
+	else:
+		print("BAD NEWS")
+
+	mydb.commit()
+	cursor.close()
+
+@app.route('/create_comp', methods = ['GET','POST'])
+def create_comp(compId):
+	cursor = mydb.cursor()
+	if request.method == 'GET':
+		return render_template('create_job.html')
+	elif request.method ==  'POST':
+		data = request.form
+		print(data)
+		sql = "INSERT INTO jobs (companyId, companyName, title, description, email, location_country, location_state, location_city) VALUES (%s,%s,%s, %s, %s, %s, %s, %s)"
+		values = (data['companyId'], data['companyName'], data['title'], data['description'], data['email'], data['location_country'], data['location_state'], data['location_city'])
+		cursor.execute(sql, values)
+		id = cursor.lastrowid
+		return redirect(url_for('.applicant_viewer', job_id = id))
+	else:
+		print("BAD NEWS")
+
+	mydb.commit()
+	cursor.close()
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	return render_template('')
 
 #@app.route('/filter')
 #def applicant_filter():
 
 
-#@app.route('/viewer'):
-#def applicant_viewer
+@app.route('/viewer/<job_id>',)
+def applicant_viewer(job_id):
+		print(job_id)
 
+
+@app.route('/viewer/<job_id>/<applicant_id>', methods = ['POST'])
+def applicant_viewer_swipe_confirm(job_id,applicant_id):
+		print(job_id, applicant_id)
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
